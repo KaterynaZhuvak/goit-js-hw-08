d71e9f192b492ba2fb0976b8970f8d7f49f8138b
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2023-08-30 10:13:23 -0400

    updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,14 +3,11 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 
 function checkStorage() {
-  const email = document.getElementById('email');
-  const message = document.getElementById('message');
- const parseForm = localStorage.getItem('feedbackFormState');
-    if (localStorage) {
-     
-        const parsedData = JSON.parse(parseForm);
-      email.value = parsedData.email;
-      message.value = parsedData.message;
+  if (localStorage) {
+      const parseForm = localStorage.getItem('feedbackFormState');
+      const parsedData = JSON.parse(parseForm);
+      form.elements.email.value = parsedData.email || '';
+      form.elements.message.value = parsedData.message || '';
     }
 }
 
@@ -36,29 +33,9 @@ form.addEventListener('submit', event => {
     if (parseForm) {
       parseForm = JSON.parse(parseForm);
       console.log(parseForm);
-      // Object.entries(parseForm).forEach(([name, value]) => {
-      //     info[name] = value;
-      //     form.elements[name].value = value;
-      // })
     }
     event.currentTarget.reset();
   }
-
-  // const formData = new FormData(form);
-  // formData.forEach((value, name) => console.log(value, name));
-
   info = {};
   localStorage.removeItem('feedbackFormState');
 });
-
-// function initForm() {
-//     let parseForm = localStorage.getItem('feedbackFormState');
-//     if (parseForm) {
-//         parseForm = JSON.parse(parseForm);
-//         console.log(parseForm);
-//         Object.entries(parseForm).forEach(([name, value]) => {
-//             info[name] = value;
-//             form.elements[name].value = value;
-//         })
-//     }
-// }
